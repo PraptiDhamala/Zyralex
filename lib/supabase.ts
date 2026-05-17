92d1eda6dc6a48dd805d0123b35700acf4a394f6
AnushaKhatri5 <anushakhatri878@gmail.com>
2026-05-17 22:44:19 +0545

    Final push of database

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,10 +4,10 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import { Platform } from "react-native";
 
-const supabaseUrl = "https://ngvwpxqbpiwnnyckelmu.supabase.co";
+const supabaseUrl = "paste url here";
 
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndndweHFicGl3bm55Y2tlbG11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2Mjg0MTEsImV4cCI6MjA5NDIwNDQxMX0.PUApPeC6E--KD65geC-FC5NOid_Q72kmEKTUhxw1Ivw";
+  "anon key here";
 
 export const supabase = createClient(
   supabaseUrl,
